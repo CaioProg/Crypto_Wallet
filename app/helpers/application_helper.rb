ad31f150eb1ec62ad1a9cc0f2b4e44f4244d9602
Caio Lucas <caioprog18@gmail.com>
2021-08-07 09:34:44 -0300

    Layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
         data_us.strftime("%d/%m/%Y")
     end
 
+    def ambiente_rails
+        if Rails.env.development?
+            "Desenvolvimento"
+        elsif Rails.env.production?
+            "Produção"
+        else
+            "Teste"
+        end
+    end
+
 end
