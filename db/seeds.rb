781ebcc86f23a87d94e03a46e221cafb326c42fc
Caio Lucas <caioprog18@gmail.com>
2021-08-08 22:15:57 -0300

    db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cadastrando moedas..."
+
+Coin.create!(
+    description: "Bitcoin",
+    acronym: "BTC",
+    url_image: "http://pngimg.com/uploads/bitcoin/small/bitcoin_PNG47.png"
+)
+Coin.create!(
+    description: "Ethereum",
+    acronym: "ETH",
+    url_image: "https://cdn.freelogovectors.net/wp-content/uploads/2020/03/ethereum-logo.png"
+)
+Coin.create!(
+    description: "Dash",
+    acronym: "DASH",
+    url_image: "https://assets.coingecko.com/coins/images/297/large/dashcoin.png?1547034071"
+)
+
+
+
+
+puts "Moedas cadastradas com sucesso!"
