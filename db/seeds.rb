c48391391a814b4127a6644f4a4f3629f3eb4db9
Caio Lucas <caioprog18@gmail.com>
2021-08-10 12:00:05 -0300

    refatorando o seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Cadastrando moedas..."
+spinner = TTY::Spinner.new("[:spinner] Cadastrando Moedas...")
+spinner.auto_spin
 
-Coin.create!(
-    description: "Bitcoin",
-    acronym: "BTC",
-    url_image: "http://pngimg.com/uploads/bitcoin/small/bitcoin_PNG47.png"
-)
-Coin.create!(
-    description: "Ethereum",
-    acronym: "ETH",
-    url_image: "https://cdn.freelogovectors.net/wp-content/uploads/2020/03/ethereum-logo.png"
-)
-Coin.create!(
-    description: "Dash",
-    acronym: "DASH",
-    url_image: "https://assets.coingecko.com/coins/images/297/large/dashcoin.png?1547034071"
-)
+coins = [
+					{
+						description: "Bitcoin",
+						acronym: "BTC",
+						url_image: "http://pngimg.com/uploads/bitcoin/small/bitcoin_PNG47.png" 
+					},
+					{
+						description: "Ethereum",
+						acronym: "ETH",
+						url_image: "https://cdn.freelogovectors.net/wp-content/uploads/2020/03/ethereum-logo.png"
+					},
+					{
+						description: "Dash",
+						acronym: "DASH",
+						url_image: "https://assets.coingecko.com/coins/images/297/large/dashcoin.png?1547034071"
+					}    
+				]
 
+coins.each do |coin|
+	sleep(1)
+	Coin.find_or_create_by!(coin)
+end
 
-
-puts "Moedas cadastradas com sucesso!"
+spinner.success("Concluído!")
