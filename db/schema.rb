6d9f7849e3e4c683ad8dfde70b2fb8448885af32
Caio Lucas <caioprog18@gmail.com>
2021-08-11 09:25:36 -0300

    Juntando as tabelas

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_120509) do
+ActiveRecord::Schema.define(version: 2021_08_11_122132) do
 
   create_table "coins", force: :cascade do |t|
     t.string "description"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_08_11_120509) do
     t.string "url_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "mining_type_id"
+    t.index ["mining_type_id"], name: "index_coins_on_mining_type_id"
   end
 
   create_table "mining_types", force: :cascade do |t|
