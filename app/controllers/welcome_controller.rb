02b6840a727f4cdda60e9935bbc8713af6bcb761
Caio Lucas <caioprog18@gmail.com>
2021-08-14 09:06:17 -0300

    COOKIES/SESSION

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails - Jackson Pires[COOKIE]"
+    session[:curso] = "Curso de Ruby on Rails - Jackson Pires[SESSION]"
   end
 end
